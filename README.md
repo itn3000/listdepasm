02f20815b5cb21bafb56e9ffccbe50619ff23041
itn3000 <itn3000@gmail.com>
2024-08-27 17:05:48 +0900

    add pack script

---
+++ README.md
@@ -0,0 +1,5 @@
+# listdepasm
+
+This is the tool for getting dependent assemblies of specified managed assembly.
+
+# Usage
--- build/Build.cs
+++ build/Build.cs
@@ -10,6 +10,11 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.Common.Tools.DotNet;
+using Microsoft.Build.Construction;
+using static Nuke.Common.IO.CompressionTasks;
+using System.IO;
 
 class Build : NukeBuild
 {
@@ -19,7 +24,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -28,17 +33,70 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(setting => setting
+                .SetProject(RootDirectory / "listdepasm.sln")
+                .SetConfiguration(Configuration));
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(setting => setting);
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(setting => setting.SetConfiguration(Configuration));
         });
-
+    [Parameter]
+    readonly string Runtime;
+    Target Publish => _ => _
+        .DependsOn(Restore)
+        .Requires(() => !string.IsNullOrEmpty(Runtime))
+        .Executes(() =>
+        {
+            var publishDir = RootDirectory / "dist" / "publish" / Configuration / Runtime;
+            var project = RootDirectory / "listdepasm" / "listdepasm.csproj";
+            DotNetPublish(setting => setting.SetConfiguration(Configuration)
+                .SetProject(project)
+                .SetProperty("EnableSingleFile", "true")
+                .SetRuntime(Runtime)
+                .SetProperty("PublishDir", publishDir));
+        });
+    AbsolutePath GetPublishDir() => RootDirectory / "dist" / "publish" / Configuration / Runtime;
+    Target Archive => _ => _
+        .Requires(() => !string.IsNullOrEmpty(Runtime))
+        .After(Publish)
+        .Executes(() =>
+        {
+            var publishDir = GetPublishDir();
+            var tmpdir = RootDirectory / "dist" / "tmp" / Configuration;
+            var destdir = RootDirectory / "dist" / "archive" / Configuration / Runtime;
+            if (tmpdir.Exists())
+            {
+                tmpdir.DeleteDirectory();
+            }
+            foreach(var f in publishDir.GetFiles())
+            {
+                if(f.Extension != ".pdb")
+                {
+                    CopyFileToDirectory(f, tmpdir, FileExistsPolicy.Overwrite);
+                }
+            }
+            tmpdir.ZipTo(destdir / $"listdepasm-{Runtime}.zip", fileMode: FileMode.Create);
+            tmpdir.TarGZipTo(destdir / $"listdepasm-{Runtime}.tgz", fileMode: FileMode.Create);
+        });
+    Target Pack => _ => _
+        .After(Restore)
+        .Executes(() =>
+        {
+            var project = RootDirectory / "listdepasm" / "listdepasm.csproj";
+            var destdir = RootDirectory / "dist" / "nupkg" / Configuration;
+            destdir.CreateOrCleanDirectory();
+            DotNetPack(setting => setting.SetProject(project)
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(destdir));
+        });
 }
